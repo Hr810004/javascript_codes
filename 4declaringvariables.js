3599fdd4e2c3ca59c6e1285cd753be3677e53022
Hr810004 <hr810004@gmail.com>
2024-06-26 17:43:21 +0530

    updated till 5th video

---
+++ 4declaringvariables.js
@@ -0,0 +1,20 @@
+const accountId = 810004
+let accountEmail = "hr810004@gmail.com" 
+var accpass = "harsh810"
+acccity = "Bharuch"
+
+//optional semi colon
+let accountstate;
+
+//not allowed in const keywords assignment
+// accountId = 8102004;
+
+//usually try to use let keyword
+accountEmail = "harsh.rce810@gmail.com" 
+console.log(accountEmail);
+
+//usually try not to use var keyword 
+accpass = "8harsh10"
+
+// if you want to print multiple things together in tbale format then use table instead of log
+console.table([accountEmail,accountId,accpass,accountstate])
---
+++ 5datatypes.js
@@ -0,0 +1,18 @@
+"use strict" //treat the code as newer version
+
+// alert("hello") 
+//we are using node js javascript runtime environment not web browser
+
+//data types
+// 1. number 
+// 2. bigint
+// 3. string ""
+// 4. boolean true/false
+// 5. null standalone value
+// 6. undefined value not assigned
+// 7. symbol uniqueness
+
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof "Harsh");
+console.log(typeof 19);
